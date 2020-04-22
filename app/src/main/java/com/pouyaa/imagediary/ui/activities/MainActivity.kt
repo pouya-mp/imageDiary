98862ccb4048bac734a470fa35ce7a15d806a7b5
Saeed Taheri <saeed.taheri@gmail.com>
2020-04-22 16:42:09 +0430

    feat: add app bar

--- app/src/main/java/com/pouyaa/imagediary/ui/activities/MainActivity.kt
+++ app/src/main/java/com/pouyaa/imagediary/ui/activities/MainActivity.kt
@@ -2,6 +2,8 @@ package com.pouyaa.imagediary.ui.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.pouyaa.imagediary.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
     }
 }
